7fb55fd3a97475b91665a16015fd01b880276632
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-10-21 20:44:51 +0300

    anonymous class used

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -4,32 +4,24 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Randoms implements Iterable<Integer> {
     private final Random random;
-    private final RandomsIterator randomsIterator;
     private final int min;
     private final int max;
 
     public Randoms(int min, int max) {
         random = ThreadLocalRandom.current();
-        randomsIterator = new RandomsIterator();
         this.min = min;
         this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return randomsIterator;
-    }
+        // Анонимный класс - действительно компактнее
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {return true;}
 
-    private class RandomsIterator implements Iterator<Integer> {
-
-        @Override
-        public boolean hasNext() {
-            return true;
-        }
-
-        @Override
-        public Integer next() {
-            return random.nextInt(min, max);
-        }
+            @Override
+            public Integer next() {return random.nextInt(min, max);}
+        };
     }
 }
